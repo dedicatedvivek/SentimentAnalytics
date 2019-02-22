777a55d599f42521e42eccd19197f76685155d1b
dedicatedvivek <vivekiyer98@gmail.com>
2019-02-22 09:39:55 +0530

    Graph_Added

---
+++ app/Emoji.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Emoji extends Model
+{
+        protected $table = 'emoji';
+    	public $timestamps = false;
+}
--- resources/views/keyword_registry.blade.php
+++ resources/views/keyword_registry.blade.php
@@ -2,6 +2,7 @@
 
 use App\Keyword;
 use App\Languages;
+use App\Emoji;
 
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
@@ -33,6 +34,19 @@ use Illuminate\Http\Request;
 <!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="Downloaded_Templates/Table_Responsive_v1/css/util.css">
 	<link rel="stylesheet" type="text/css" href="Downloaded_Templates/Table_Responsive_v1/css/main.css">
+
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.3/Chart.js"></script>
+	<script src="/../vendor/jquery/jquery-3.2.1.min.js"></script>
+
+	
+<!--===============================================================================================-->
+	<script src="vendor/bootstrap/js/popper.js"></script>
+	<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/select2/select2.min.js"></script>
+<!--===============================================================================================-->
+	<script src="js/main.js"></script>
+
 <!--===============================================================================================-->
 <style>
 .sub_button{
@@ -60,6 +74,8 @@ use Illuminate\Http\Request;
 
 
 
+
+
 </style>
 
 </head>
@@ -70,7 +86,7 @@ use Illuminate\Http\Request;
 					  where('active_status',1)->
 					  get();
 
-	echo ($keylang_select);
+	
 
 	$keylang_array_count = count($keylang_select);
 	
@@ -88,7 +104,8 @@ use Illuminate\Http\Request;
 								<th class ="column1">Serial No</th>
 								<th class="column2">Keyword</th>
 								<th class="column3">Language</th>
-								<th class="column4">Delete</th>
+								<th class="column4">Analytics</th>
+								<th class="column5">Delete</th>
 								
 							</tr>
 						</thead>
@@ -113,7 +130,16 @@ use Illuminate\Http\Request;
 									echo"</td>";
 									echo"<td class='column2'>$keyword</td>";
 									echo"<td class='column3'>$language</td>";
+									
 									echo"<td class='column4'>";
+									echo "<form method='post' action='#'>";
+									// echo "{{ csrf_field() }}";
+									echo "<input type='hidden' name= '_token' value='".csrf_token()."' /><input type='hidden' class='abut' name= 'show_id' value='$serial' /><button class='sub_butt' type='submit' value='Show Analytics' name='showbut' onclick='showmodal()' >Show Analytics</button>";
+									echo "</form>";
+									echo"</td>";
+
+
+									echo"<td class='column5'>";
 									echo "<form method='post' action='delete_keyword'>";
 									// echo "{{ csrf_field() }}";
 									echo "<input type='hidden' name= '_token' value='".csrf_token()."' /><input type='hidden' name= 'del_id' value='$serial' /><input class='sub_button' type='submit' value='X' />";
@@ -139,18 +165,80 @@ use Illuminate\Http\Request;
 		</div>
 	</div>
 
+<div id="myModal" class="modal">
 
-	
+	<div class="modal-content">
+    <span class="close">&times;</span>
+
+    
+
+  </div>
+</div>
+
+<!-- <?php
+    
+    		// $show_id = $_POST["show_id"];
+    		// $query_emoji = Emoji::select('emoji','count')->
+    		// where('fo_key_id',1)->
+    		// get();
+
+    		// $count_emojis = count($query_emoji);
+
+    		// for ($i=0; $i < $count_emojis ; $i++) { 
+    		
+    		// $query_emoji_val = $query_emoji[$i];
+
+    		// echo "<span name='data' id='$i'"
+
+
+
+    		
+    		
+
+?> -->
+<canvas id="myChart" width="400" height="400"></canvas>
+<script>
+var ctx = document.getElementById("myChart").getContext('2d');
+var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 69, 55, 7, 12, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255,99,132,1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero:true
+                }
+            }]
+        }
+    }
+});
+</script>
+    	
 
 <!--===============================================================================================-->	
-	<script src="vendor/jquery/jquery-3.2.1.min.js"></script>
-<!--===============================================================================================-->
-	<script src="vendor/bootstrap/js/popper.js"></script>
-	<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
-<!--===============================================================================================-->
-	<script src="vendor/select2/select2.min.js"></script>
-<!--===============================================================================================-->
-	<script src="js/main.js"></script>
 
 
 
